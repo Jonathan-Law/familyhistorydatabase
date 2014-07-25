89d79909d89d3e196919cf8484bbd098515bcd36
Jonathan Law <jonathan.q.law@gmail.com>
2014-07-25 01:28:10 -0600

    Fixed some jshint errors and finalized the focus for input fields.

--- client/familyhistorydatabase/app/scripts/directives/mymodal.js
+++ client/familyhistorydatabase/app/scripts/directives/mymodal.js
@@ -48,7 +48,7 @@ app.directive('myModal', ['$timeout', function ($timeout) {
     restrict: 'AE',
     scope: {},
     template: '<div class="modal fade {{classes}}" id="{{id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> <div ng-include="header"></div> </div> <div class="modal-body"> <div ng-include src="body"></div> </div> <div ng-show="showFooter" class="modal-footer"> <div ng-include="footer"></div> </div> </div> </div </div>',
-    link: function postLink(scope, element, attrs) {
+    link: function postLink(scope, element, attrs) { /*jshint unused:false*/
       scope.header = 'views/modalDefaults/header.html';
 
       scope.footer = 'views/modalDefaults/footer.html';
@@ -59,30 +59,30 @@ app.directive('myModal', ['$timeout', function ($timeout) {
 
       scope.nav = null;
 
-      scope.$on('$triggerModal', function (event, content) {
+      scope.$on('$triggerModal', function (event, content) { /*jshint unused:false*/
         if (content) {
           $('#'+scope.id).modal('show');
           if (content.showFooter === false) {
             scope.showFooter = false;
           }
           if (content.classes){
-            scope.classes = content.classes.join(" ");
+            scope.classes = content.classes.join(' ');
           }
           if (content.nav) {
-            scope.nav = content.nav
+            scope.nav = content.nav;
           } else {
             if (content.modalTitle) {
-              scope.modalTitle = content.modalTitle
+              scope.modalTitle = content.modalTitle;
             }
             if (content.modalBody) {
-              scope.modalBody = content.modalBody
+              scope.modalBody = content.modalBody;
             }
           }
         } else {
           console.error('A Content object is required for the modal to work');
         }
       });
-      scope.$on('$triggerModalClose', function (event) {
+      scope.$on('$triggerModalClose', function (event) { /*jshint unused:false*/
         $('#'+scope.id).modal('hide');
       });
 
@@ -98,7 +98,7 @@ app.directive('myModal', ['$timeout', function ($timeout) {
             element.find('[autofocus]').focus();
           });
         });
-      })
+      });
 
     }
   };
