e0eb6c6676732130ce8de70c28f5abae07950823
unknown <jlaw@C00823.usurf.usu.edu>
2014-12-12 15:59:52 -0700

    Fixed the loading of images and width issue on the family tree.

--- client/familyhistorydatabase/app/scripts/directives/individual/family.js
+++ client/familyhistorydatabase/app/scripts/directives/individual/family.js
@@ -35,17 +35,42 @@ app.directive('family', ['business', '$timeout', '$compile', function (Business,
       Business.individual.getFamily(scope.personId).then(function(family){
         console.log('family', family);
         scope.family = family? family: [];
-        var list = element.find(".tree");
+        var base = element.find("#treeHolder");
+        var list = base.find('.tree');
         var root = list.append('<ul></ul>').find('ul');
         var person = root.append('<li><a><individual person="'+scope.personId+'" mode="picture"></individual></a></li>').find('li');
         addParents(family.parents, person);
-        var e = angular.element(list);
+        var e = angular.element(base);
         $compile(e.contents())(scope);
         element.replaceWith(e);
+        var allImgs = document.getElementById("treeHolder").getElementsByTagName("img");
+        var allImgsLength = allImgs.length;
+        var i;
+        var eventCallback = function() {
+          if ( !(--allImgsLength)) {
+            $('.tree').css('width', 6000);
+            setTimeout(function(){
+              $('.tree').css('width', $('.tree').find('ul').find('li').width() + 100);
+            })
+          }
+        };
+
+        for (i = 0; i < allImgsLength; i++) {
+          if (allImgs[i].complete) {
+            allImgsLength--;
+          }
+          if (allImgs[i].addEventListener) {
+            allImgs[i].addEventListener("load", eventCallback);
+          } else if (allImgs[i].attachEvent) {
+            allImgs[i].attachEvent("onload", eventCallback);
+          } else {
+            allImgs[i].onload = eventCallback;
+          }
+        }
       }, function(){
         scope.family = [];
       });
-
+      //
     }
   };
 }]);
